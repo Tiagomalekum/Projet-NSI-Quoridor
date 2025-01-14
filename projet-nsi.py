70a17cd63939cb875d631dbbab9a2a6697f7aa5f
Guill-af <afg77756@gmail.com>
2025-01-14 10:51:33 +0100

    Add files via upload

---
+++ projet-nsi.py
@@ -0,0 +1,16 @@
+###########################################
+#          PROJET NSI: Quoridor           #
+###########################################
+
+
+"1)Qui fait quoi?
+
+# Guillaume Afonso: Carnet de projet + code
+# Gabriel Saglier: code
+# Christmann Tiago: code
+
+"2)Quel est notre projet?
+
+# Notre projet est basé sur le jeu de strategie quridor
+
+
