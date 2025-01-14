1dcc7a5c06b2d6cc16cf339c59e13000d9793064
Guill-af <afg77756@gmail.com>
2025-01-14 10:59:17 +0100

    Update projet-nsi.py

--- projet-nsi.py
+++ projet-nsi.py
@@ -12,5 +12,17 @@
 "2)Quel est notre projet?
 
 # Notre projet est basé sur le jeu de strategie quridor
+# dont je vais commencer par expliquer les règles.
+
+# A tour de rôle, chacun déplace son pion d'une case ou pose une barrière afin de ralentir l'adversaire. 
+# Les pions doivent contourner les barrières, qui créent un labyrinthe dont il faut sortir très vite, 
+# car le premier qui atteint la ligne opposée avec son pion a gagné la partie!
+# Le jeu se joue a plusieurs(2, 3, 4,....)
+
+
+
+"(mettre nombre)Code
+
+
 
 
