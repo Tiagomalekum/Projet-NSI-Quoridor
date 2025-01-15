48a60dc5b48ea9de0c1f899dd3fab181add0c0da
Guill-af <afg77756@gmail.com>
2025-01-15 16:35:25 +0100

    Update projet-nsi.py

--- projet-nsi.py
+++ projet-nsi.py
@@ -1,5 +1,5 @@
 ###########################################
-#          PROJET NSI: Quoridor           #
+#          PROJET NSI: jeu du Quoridor    #
 ###########################################
 
 
@@ -11,17 +11,19 @@
 
 "2)Quel est notre projet?
 
-# Notre projet est basé sur le jeu de strategie quridor
+# Notre projet est basé sur le jeu de strategie Quoridor
 # dont je vais commencer par expliquer les règles.
 
 # A tour de rôle, chacun déplace son pion d'une case ou pose une barrière afin de ralentir l'adversaire. 
 # Les pions doivent contourner les barrières, qui créent un labyrinthe dont il faut sortir très vite, 
 # car le premier qui atteint la ligne opposée avec son pion a gagné la partie!
-# Le jeu se joue a plusieurs(2, 3, 4,....)
+# Le jeu se joue a plusieurs(2, 3, 4,....) et le nombre de case peut varier selon l'envie de jouer
+# une partie courte, moyenne ou longue. (5*5, 7*7, 9*9,....)
 
 
 
-"(mettre nombre)Code
+
+"(mettre nombre)Explication Code:
 
 
 
