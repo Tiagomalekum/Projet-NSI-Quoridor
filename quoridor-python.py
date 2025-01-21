185eb169e6b057f864bb73b5dce6bba51d609004
Tiagomalekum <tiagochristmann83140@gmail.com>
2025-01-21 11:45:39 +0100

    Update quoridor-python.py

--- quoridor-python.py
+++ quoridor-python.py
@@ -1,17 +1,16 @@
 # Initialisation du plateau de jeu
-def init_board():
-    board_size = 9
-    board = [[' ' for _ in range(board_size)] for _ in range(board_size)]
-    players = {'A': (0, board_size // 2), 'B': (board_size - 1, board_size // 2)}
-    board[players['A'][0]][players['A'][1]] = 'A'
-    board[players['B'][0]][players['B'][1]] = 'B'
-    return board, players
+def init_tableau():
+    taille_tableau = 9
+    tableau = [[' ' for _ in range(taille_tableau)] for _ in range(taille_tableau)]
+    joueurs = {'1': (0, taille_tableau // 2), '2': (taille_tableau - 1, taille_tableau // 2)} #
+    tableau[players['1'][0]][joueurs['A'][1]] = '1'
+    tableau[players['2'][0]][joueurs['B'][1]] = '2'
 
 # Affichage du plateau de jeu
-def print_board(board):
-    for row in board:
-        print('+---' * len(board) + '+')
-        print('| ' + ' | '.join(row) + ' |')
-    print('+---' * len(board) + '+')
-
-Tiago et Gabriel n'oubliez pas d'expliquer le code.
+def print_tableau(tableau):
+    for row in tableau:
+        print('+---' * len(tableau) + '+')
+        print('| ' + ' | '.join(tableau) + ' |')
+    print('+---' * len(tableau) + '+')
+    return taleau, joueurs
+#Tiago et Gabriel n'oubliez pas d'expliquer le code.
