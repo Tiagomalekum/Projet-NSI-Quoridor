6d6f5a95a8c0b3ec174556fb4699ad082f13e72a
Tiagomalekum <tiagochristmann83140@gmail.com>
2025-01-23 11:51:48 +0100

    Update quoridor-python.py

--- quoridor-python.py
+++ quoridor-python.py
@@ -1,14 +1,13 @@
 # Initialisation du plateau de jeu
 def init_tableau():
-    taille_tableau = int(input())
-    tableau = [['X' for _ in range(taille_tableau)] for _ in range(taille_tableau)]
-    tableau[[0][taille_tableau / 2]] = '1'
-    tableau[[taille_tableau - 1][taille_tableau / 2]] = '2'
+    print("entrer valeur neuil")
+    tableau = [['X' for _ in range(5)] for _ in range(5)]
+    tableau[0][3] = 'A'
+    tableau[4][3] = 'B'
+    affichage_tableau()
 
-# Affichage du plateau de jeu
-def print_tableau(tableau):
+def affichage_tableau():
     print(tableau)
-
 #Tiago et Gabriel n'oubliez pas d'expliquer le code.
-# Tester indépendamment chaque script que vous créer
-# pour vérifier que tout fonctionne comme il faut.
+#Tester indépendamment chaque script que vous créer
+#pour vérifier que tout fonctionne comme il faut.
