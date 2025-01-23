dc83e1de4b8380d9cc761deb52d8284ad83bddc4
Tiagomalekum <tiagochristmann83140@gmail.com>
2025-01-23 11:19:14 +0100

    Update quoridor-python.py

--- quoridor-python.py
+++ quoridor-python.py
@@ -1,18 +1,14 @@
 # Initialisation du plateau de jeu
 def init_tableau():
-    taille_tableau = 5
-    tableau = [[' ' for _ in range(taille_tableau)] for _ in range(taille_tableau)]
-    joueurs = {'1': (0, taille_tableau // 2), '2': (taille_tableau - 1, taille_tableau // 2)} #Positions initiales joueurs
-    tableau[players['1'][0]][joueurs['A'][1]] = '1'
-    tableau[players['2'][0]][joueurs['B'][1]] = '2'
+    taille_tableau = int(input())
+    tableau = [['X' for _ in range(taille_tableau)] for _ in range(taille_tableau)]
+    tableau[[0][taille_tableau / 2]] = '1'
+    tableau[[taille_tableau - 1][taille_tableau / 2]] = '2'
 
 # Affichage du plateau de jeu
 def print_tableau(tableau):
-    for row in tableau:
-        print('+---' * len(tableau) + '+')
-        print('| ' + ' | '.join(tableau) + ' |')
-    print('+---' * len(tableau) + '+')
-    return taleau, joueurs
+    print(tableau)
+
 #Tiago et Gabriel n'oubliez pas d'expliquer le code.
 # Tester indépendamment chaque script que vous créer
 # pour vérifier que tout fonctionne comme il faut.
