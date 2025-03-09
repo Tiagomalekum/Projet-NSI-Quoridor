8f927541a00417a7f721bff8efeb74662b827f60
Tiagomalekum <tiagochristmann83140@gmail.com>
2025-03-09 13:08:40 +0100

    Add files via upload

---
+++ jeu-de-hex/hex_jeu/constants.py
@@ -0,0 +1,14 @@
+import pygame
+import os
+
+larg, longu = 800,800
+ligne, col = 5,5
+hexa = larg//ligne
+
+rouge = (236,0,0)
+bleu = (0,0,236)
+blanc = (0,0,0)
+gris = (200,200,200)
+
+#PROBLEME: normallement path = "hex_jeu\images_hex"
+path = "0"
