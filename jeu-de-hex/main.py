67f2df353dcf9422dea54c2b2423624e2607ac2d
Tiagomalekum <tiagochristmann83140@gmail.com>
2025-03-14 14:03:08 +0100

    Update main.py

--- jeu-de-hex/main.py
+++ jeu-de-hex/main.py
@@ -1,14 +1,36 @@
-jeu = [
-      [1,2,1,2,9,2,1,2,1],
-      [2,2,2,2,2,2,2,2,2],
-      [1,2,1,2,1,2,1,2,1],
-      [2,2,2,2,2,2,2,2,2],
-      [1,2,1,2,1,2,1,2,1],
-      [2,2,2,2,2,2,2,2,2],
-      [1,2,1,2,1,2,1,2,1],
-      [2,2,2,2,2,2,2,2,2],
-      [1,2,1,2,6,2,1,2,1]
-      ]
+# Définir la taille de la grille
+n = 5
 
-# grille 5x5, 1 = hexagone, 2 = emplacement mur vide
-#6 = joueur 1, 9 = joueur 2
+# Représentation du plateau, chaque case est un dictionnaire
+# qui contient les voisins possibles sous forme de tuples de coordonnées
+board = []
+
+for i in range(n):
+    row = []
+    for j in range(n):
+        neighbors = []
+        
+        # Ajouter des voisins basés sur des règles de voisinage pour un hexagone
+        if i > 0:  # voisin en haut
+            neighbors.append((i-1, j))
+        if i < n-1:  # voisin en bas
+            neighbors.append((i+1, j))
+        if j > 0:  # voisin à gauche
+            neighbors.append((i, j-1))
+        if j < n-1:  # voisin à droite
+            neighbors.append((i, j+1))
+        if i > 0 and j < n-1:  # voisin en haut à droite
+            neighbors.append((i-1, j+1))
+        if i < n-1 and j > 0:  # voisin en bas à gauche
+            neighbors.append((i+1, j-1))
+        
+        row.append({
+            "position": (i, j),
+            "neighbors": neighbors
+        })
+    board.append(row)
+
+# Affichage du plateau avec les voisins
+for row in board:
+    for cell in row:
+        print(f"Case {cell['position']} : Voisins -> {cell['neighbors']}")
